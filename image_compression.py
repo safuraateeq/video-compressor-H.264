e370856ed97a02c1d0f36181e7980bf0e11fa82b
Safura Ateeq <58217569+safuraateeq@users.noreply.github.com>
2023-03-22 23:43:58 +0300

    Updated

---
+++ image_compression.py
@@ -0,0 +1,34 @@
+from image_compression.compress import compress, decompress, plot_performance
+from image_compression.downsample_compress_upsample import (
+    downsample_compress,
+    decompress_upsample,
+    plot_downsample_compress_upsample_performance,
+)
+from image_compression.utils import plot_comparision
+
+
+# Compress - Decompress
+print("Compressing to H.264 Video")
+compress("./input/pedestrian_area25fps.yuv")
+
+print("Decompressing from H.264 Video")
+decompress()
+
+print("Plotting performance")
+compress_performance = plot_performance()
+
+
+# Downsample - Compress - Decompress - Upsample
+print("Downsampling and Compressing to H.264")
+downsample_compress("./input/pedestrian_area25fps.yuv")
+
+print("Decompressing from H.264 and upsampling")
+decompress_upsample()
+
+print("Plotting performance")
+downsample_compress_upsample_performance = plot_downsample_compress_upsample_performance()
+
+
+# Comparision
+print("\n\nPlotting comparision\n\n")
+plot_comparision(compress_performance, downsample_compress_upsample_performance)
